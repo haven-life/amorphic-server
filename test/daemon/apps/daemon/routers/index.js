bb8b8b46ac4443ec9f5c073a7f8c9bbab9edc42b
Shanks <shanks@havenlife.com>
2018-11-14 16:36:05 -0500

    renaming router conventions

--- test/daemon/apps/daemon/routers/index.js
+++ test/daemon/apps/daemon/routers/index.js
@@ -1,16 +1,24 @@
 'use strict';
 
 // intake an express router object, mutate it with custom endpoints, send it back to amorphic to register.
-function routerSetup(expressRouter) {
+function firstEndpoint(expressRouter) {
     expressRouter.get('/test', testService.bind(this));
 
     return expressRouter;
 }
+
+function secondEndpoint(expressRouter) {
+    expressRouter.get('/test_other_endpoint', testService.bind(this));
+
+    return expressRouter;
+}
+
 
 function testService (_req, res) {
     res.status(200).send('test API endpoint OK');
 }
 
 module.exports = {
-    routerSetup: routerSetup
+    firstEndpoint: firstEndpoint,
+    secondEndpoint: secondEndpoint
 };
