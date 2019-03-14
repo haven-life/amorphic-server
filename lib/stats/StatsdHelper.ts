5fba2872e7d2c158805b41e476b6d76ba727d3b7
Nicholas Saenz <nick@havenlife.com>
2019-03-14 13:48:22 -0400

    init go at statsd for timing

---
+++ lib/stats/StatsdHelper.ts
@@ -0,0 +1,12 @@
+type hrTime = [number, number];
+
+export function convertHRTimeToMilliseconds(hrTime: hrTime): number {
+    return hrTime[0] * 1000 + hrTime[1] / 1000000;
+}
+
+export function computeTimingAndSend(hrTimeStart: hrTime, statsdClient, statsKey, tags?): number {
+    const processMessageEndTime = process.hrtime(hrTimeStart);
+    const totalTimeInMilliseconds = convertHRTimeToMilliseconds(processMessageEndTime);
+    statsdClient.timing(statsKey, totalTimeInMilliseconds);
+    return totalTimeInMilliseconds;
+}
