fab52f43dab43b441236d95723871aa2bde83c10
Nicholas Saenz <nick@havenlife.com>
2019-03-21 00:17:53 -0400

    rest of the session utils timed, also fix to add tags to base function

--- lib/stats/StatsdHelper.ts
+++ lib/stats/StatsdHelper.ts
@@ -7,6 +7,6 @@ export function convertHRTimeToMilliseconds(hrTime: hrTime): number {
 export function computeTimingAndSend(hrTimeStart: hrTime, statsdClient, statsKey, tags?): number {
     const processMessageEndTime = process.hrtime(hrTimeStart);
     const totalTimeInMilliseconds = convertHRTimeToMilliseconds(processMessageEndTime);
-    statsdClient.timing(statsKey, totalTimeInMilliseconds);
+    statsdClient.timing(statsKey, totalTimeInMilliseconds, tags);
     return totalTimeInMilliseconds;
 }
